946ff75735c80a4aff088949246e08d895775738
eldooon <eldonchan91@gmail.com>
2016-06-22 10:13:59 -0400

    done

---
+++ HellaCells/FISHellaDetailViewController.h
@@ -0,0 +1,14 @@
+//
+//  FISHellaDetailViewController.h
+//  HellaCells
+//
+//  Created by Eldon Chan on 6/22/16.
+//  Copyright © 2016 FIS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FISHellaDetailViewController : UIViewController
+@property (nonatomic) NSUInteger number;
+
+@end
---
+++ HellaCells/FISHellaMasterTableViewController.h
@@ -0,0 +1,14 @@
+//
+//  FISHellaMasterTableViewController.h
+//  HellaCells
+//
+//  Created by Eldon Chan on 6/22/16.
+//  Copyright © 2016 FIS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "FISHellaDetailViewController.h"
+
+@interface FISHellaMasterTableViewController : UITableViewController
+
+@end
